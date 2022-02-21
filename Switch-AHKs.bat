dee8ec88b3e6526b67e7bb145948f291db2e06de
kenkenpa198 <kenkenpa198@gmail.com>
2022-02-22 02:33:43 +0900

    [update]切り替え用batを更新

--- Switch-AHKs.bat
+++ Switch-AHKs.bat
@@ -1,19 +1,24 @@
 ::::: KeM-AHKs の切り替え用 bat ::::: 
 :: KeM-AHKs 通常版とJIS版をトグル的に入れ替え 
-
-:: TODO 
-:: どちらも起動していない・どちらも起動してしまっている場合の処理を入れる 
+:: どちらも起動していない場合は通常版を起動する 
 
 @echo off
 chcp 65001
 
 tasklist | find "KeM-AHKs.exe" > NUL
 if %ERRORLEVEL% == 0 (
-  taskkill /F /IM KeM-AHKs.exe
-  start KeM-AHKs-forJIS.exe
+    echo --- Switch to forJIS ---
+    echo;
+    taskkill /F /IM KeM-AHKs.exe
+    start KeM-AHKs-forJIS.exe
 ) else (
-  taskkill /F /IM KeM-AHKs-forJIS.exe
-  start KeM-AHKs.exe
+    echo --- Switch to Normal ---
+    echo;
+    tasklist | find "KeM-AHKs-forJIS.exe" > NUL
+    if not %ERRORLEVEL% == 0 (
+        taskkill /F /IM KeM-AHKs-forJIS.exe
+    )
+    start KeM-AHKs.exe
 )
 
 timeout /t 2
