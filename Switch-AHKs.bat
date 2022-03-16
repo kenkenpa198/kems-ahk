a6bc99f4d2da142276fe508cfe24283c030d11fe
kenkenpa198 <kenkenpa198@gmail.com>
2022-03-16 11:43:54 +0900

    [update]カレントディレクトリ設定などを bat へ追加。他微調整。

--- Switch-AHKs.bat
+++ Switch-AHKs.bat
@@ -2,7 +2,13 @@
 :: KeM-AHKs 通常版とJIS版をトグル的に入れ替え 
 :: どちらも起動していない場合は通常版を起動する 
 
+:: 標準出力へコマンドを表示しない
 @echo off
+:: カレントディレクトリを再設定
+cd /d %~dp0
+:: 環境変数をローカル化
+setlocal
+:: 文字コードを設定
 chcp 65001
 
 tasklist | find "KeM-AHKs.exe" > NUL
